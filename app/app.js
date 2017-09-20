b7783da25e2a2fd72f4b95fda1134c94399c8f67
Sam Hertig <sam@samhertig.com>
2017-09-20 14:23:58 +0200

    Switch to ES6

--- app/app.js
+++ app/app.js
@@ -1,13 +1,13 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-let myApp = angular.module('myApp', [
+const myApp = angular.module('myApp', [
     'ngRoute',
     'myApp.view1',
     'myApp.view2',
     'myApp.version'
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+config(['$locationProvider', '$routeProvider', ($locationProvider, $routeProvider) => {
     //$locationProvider.hashPrefix('!');
     $routeProvider
     .when('/view1', {
