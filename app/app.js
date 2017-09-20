df484f0684a9537fd75d85af7e2a09466f764bbd
Sam Hertig <sam@samhertig.com>
2017-09-20 13:31:38 +0200

    Refactor routing

--- app/app.js
+++ app/app.js
@@ -1,14 +1,22 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+var myApp = angular.module('myApp', [
+    'ngRoute',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-
-  $routeProvider.otherwise({redirectTo: '/view1'});
+    //$locationProvider.hashPrefix('!');
+    $routeProvider
+    .when('/view1', {
+        templateUrl: 'view1/view1.html',
+        controller: 'View1Ctrl'
+    })
+    .when('/view2', {
+        templateUrl: 'view2/view2.html',
+        controller: 'View2Ctrl'
+    })
+    .otherwise({redirectTo: '/view1'});
 }]);
